3c0bb5c47de49f80fb38fac722808c879edd7292
GabrielAGES <149537836+GabrielAGES@users.noreply.github.com>
2026-02-05 13:33:49 -0500

    Funciones y clases

---
+++ Repo/Repaso/Assets/_Main/Scripts/Clases.cs
@@ -0,0 +1,4 @@
+public class Clases : MonoBehaviour
+{
+    public string[] id = new string["Rober", "Maria", "Juan", "Santiago", "Pepe"];
+}
---
+++ Repo/Repaso/Assets/_Main/Scripts/funciones.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private int numeroEntero = 2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Clases ListaEstudiantes = new Clases();
+    void Start()
+    {
+        Myfunction();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Myfunction();
+    }
+    public void Myfunction()
+    {
+        //Escribir en la consola el estudiante con el index #2
+        Debug.Log(ListaEstudiantes.id[2]);
+    }
+    public int Suma(int a, int b)
+    {
+        
+    }
+}
