307724fcf1d2a55ebc31014a77cb4b9ebf9a237b
GabrielAGES <cosasudemga@gmail.com>
2026-02-24 23:03:53 -0500

    Calculadora clase

---
+++ Repo/Repaso/Assets/_Main/Scripts/Calculadora/Operaciones.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class Operaciones : MonoBehaviour
+{
+    public int Suma(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int Resta(int a, int b)
+    {
+        return a - b;
+    }
+
+    public int Multiplicacion(int a, int b)
+    {
+        return a * b;
+    }
+
+    public int Division(int a, int b)
+    {
+        return a / b;
+    }
+
+    
+    void Start()
+    {
+        
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Multiplicar()
+    {
+        
+    }
+}
